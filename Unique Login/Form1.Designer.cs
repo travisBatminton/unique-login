0a07b2035031e36ed23f2bd6b245ed0575a18558
wajeht <zombyard@gmail.com>
2020-07-20 04:46:11 -0500

    Added basic buttons and text box

--- Unique Login/Form1.Designer.cs
+++ Unique Login/Form1.Designer.cs
@@ -28,63 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.cbox_adjective = new System.Windows.Forms.CheckBox();
+            this.cbox_animal = new System.Windows.Forms.CheckBox();
+            this.cbox_verb = new System.Windows.Forms.CheckBox();
+            this.cbox_noun = new System.Windows.Forms.CheckBox();
+            this.tbox_username = new System.Windows.Forms.TextBox();
+            this.tbox_password = new System.Windows.Forms.TextBox();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.btn_copy_username = new System.Windows.Forms.Button();
+            this.btn_copy_password = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.lbl_length_of_password = new System.Windows.Forms.Label();
+            this.lbl_preference = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // cbox_adjective
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(84, 55);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(176, 36);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.cbox_adjective.AutoSize = true;
+            this.cbox_adjective.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_adjective.Location = new System.Drawing.Point(168, 168);
+            this.cbox_adjective.Margin = new System.Windows.Forms.Padding(1);
+            this.cbox_adjective.Name = "cbox_adjective";
+            this.cbox_adjective.Size = new System.Drawing.Size(84, 21);
+            this.cbox_adjective.TabIndex = 0;
+            this.cbox_adjective.Text = "Adjective";
+            this.cbox_adjective.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // cbox_animal
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(538, 183);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(176, 36);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.cbox_animal.AutoSize = true;
+            this.cbox_animal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_animal.Location = new System.Drawing.Point(168, 88);
+            this.cbox_animal.Margin = new System.Windows.Forms.Padding(1);
+            this.cbox_animal.Name = "cbox_animal";
+            this.cbox_animal.Size = new System.Drawing.Size(69, 21);
+            this.cbox_animal.TabIndex = 1;
+            this.cbox_animal.Text = "Animal";
+            this.cbox_animal.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // cbox_verb
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(522, 55);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(176, 36);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.cbox_verb.AutoSize = true;
+            this.cbox_verb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_verb.Location = new System.Drawing.Point(168, 116);
+            this.cbox_verb.Margin = new System.Windows.Forms.Padding(1);
+            this.cbox_verb.Name = "cbox_verb";
+            this.cbox_verb.Size = new System.Drawing.Size(57, 21);
+            this.cbox_verb.TabIndex = 2;
+            this.cbox_verb.Text = "Verb";
+            this.cbox_verb.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // cbox_noun
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(327, 72);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(176, 36);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.cbox_noun.AutoSize = true;
+            this.cbox_noun.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_noun.Location = new System.Drawing.Point(168, 143);
+            this.cbox_noun.Margin = new System.Windows.Forms.Padding(1);
+            this.cbox_noun.Name = "cbox_noun";
+            this.cbox_noun.Size = new System.Drawing.Size(61, 21);
+            this.cbox_noun.TabIndex = 3;
+            this.cbox_noun.Text = "Noun";
+            this.cbox_noun.UseVisualStyleBackColor = true;
+            // 
+            // tbox_username
+            // 
+            this.tbox_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbox_username.Location = new System.Drawing.Point(168, 240);
+            this.tbox_username.Margin = new System.Windows.Forms.Padding(1);
+            this.tbox_username.Name = "tbox_username";
+            this.tbox_username.Size = new System.Drawing.Size(162, 23);
+            this.tbox_username.TabIndex = 4;
+            this.tbox_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbox_password
+            // 
+            this.tbox_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbox_password.Location = new System.Drawing.Point(168, 278);
+            this.tbox_password.Margin = new System.Windows.Forms.Padding(1);
+            this.tbox_password.Name = "tbox_password";
+            this.tbox_password.Size = new System.Drawing.Size(162, 23);
+            this.tbox_password.TabIndex = 5;
+            this.tbox_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_generate.Location = new System.Drawing.Point(168, 329);
+            this.btn_generate.Margin = new System.Windows.Forms.Padding(1);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(95, 28);
+            this.btn_generate.TabIndex = 6;
+            this.btn_generate.Text = "Generate!";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            // 
+            // btn_copy_username
+            // 
+            this.btn_copy_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_copy_username.Location = new System.Drawing.Point(346, 236);
+            this.btn_copy_username.Margin = new System.Windows.Forms.Padding(1);
+            this.btn_copy_username.Name = "btn_copy_username";
+            this.btn_copy_username.Size = new System.Drawing.Size(42, 22);
+            this.btn_copy_username.TabIndex = 7;
+            this.btn_copy_username.Text = "Copy";
+            this.btn_copy_username.UseVisualStyleBackColor = true;
+            // 
+            // btn_copy_password
+            // 
+            this.btn_copy_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_copy_password.Location = new System.Drawing.Point(346, 275);
+            this.btn_copy_password.Margin = new System.Windows.Forms.Padding(1);
+            this.btn_copy_password.Name = "btn_copy_password";
+            this.btn_copy_password.Size = new System.Drawing.Size(42, 22);
+            this.btn_copy_password.TabIndex = 8;
+            this.btn_copy_password.Text = "Copy";
+            this.btn_copy_password.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(168, 207);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(1);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(159, 23);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_username.Location = new System.Drawing.Point(81, 243);
+            this.lbl_username.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(77, 17);
+            this.lbl_username.TabIndex = 10;
+            this.lbl_username.Text = "Username:";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.Location = new System.Drawing.Point(81, 278);
+            this.lbl_password.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(73, 17);
+            this.lbl_password.TabIndex = 11;
+            this.lbl_password.Text = "Password:";
+            // 
+            // lbl_length_of_password
+            // 
+            this.lbl_length_of_password.AutoSize = true;
+            this.lbl_length_of_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_length_of_password.Location = new System.Drawing.Point(22, 206);
+            this.lbl_length_of_password.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl_length_of_password.Name = "lbl_length_of_password";
+            this.lbl_length_of_password.Size = new System.Drawing.Size(136, 17);
+            this.lbl_length_of_password.TabIndex = 12;
+            this.lbl_length_of_password.Text = "Length of password:";
+            // 
+            // lbl_preference
+            // 
+            this.lbl_preference.AutoSize = true;
+            this.lbl_preference.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_preference.Location = new System.Drawing.Point(29, 88);
+            this.lbl_preference.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl_preference.Name = "lbl_preference";
+            this.lbl_preference.Size = new System.Drawing.Size(129, 17);
+            this.lbl_preference.TabIndex = 13;
+            this.lbl_preference.Text = "Names preference:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(852, 538);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(425, 391);
+            this.Controls.Add(this.lbl_preference);
+            this.Controls.Add(this.lbl_length_of_password);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_username);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_copy_password);
+            this.Controls.Add(this.btn_copy_username);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.tbox_password);
+            this.Controls.Add(this.tbox_username);
+            this.Controls.Add(this.cbox_noun);
+            this.Controls.Add(this.cbox_verb);
+            this.Controls.Add(this.cbox_animal);
+            this.Controls.Add(this.cbox_adjective);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Form1";
             this.Text = "Unique Login";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,10 +230,20 @@
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox cbox_adjective;
+        private System.Windows.Forms.CheckBox cbox_animal;
+        private System.Windows.Forms.CheckBox cbox_verb;
+        private System.Windows.Forms.CheckBox cbox_noun;
+        private System.Windows.Forms.TextBox tbox_username;
+        private System.Windows.Forms.TextBox tbox_password;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.Button btn_copy_username;
+        private System.Windows.Forms.Button btn_copy_password;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Label lbl_length_of_password;
+        private System.Windows.Forms.Label lbl_preference;
     }
 }
 
