50a9017cad178d730056da4a06ec882fcff739a7
wajeht <zombyard@gmail.com>
2020-07-20 04:57:59 -0500

    Added username and password preferences

--- Unique Login/Form1.Designer.cs
+++ Unique Login/Form1.Designer.cs
@@ -41,18 +41,23 @@
             this.lbl_username = new System.Windows.Forms.Label();
             this.lbl_password = new System.Windows.Forms.Label();
             this.lbl_length_of_password = new System.Windows.Forms.Label();
-            this.lbl_preference = new System.Windows.Forms.Label();
+            this.lbl_username_preference = new System.Windows.Forms.Label();
+            this.lbl_password_preference = new System.Windows.Forms.Label();
+            this.cbox_uppercase = new System.Windows.Forms.CheckBox();
+            this.cbox_lowercase = new System.Windows.Forms.CheckBox();
+            this.cbox_number = new System.Windows.Forms.CheckBox();
+            this.cbox_special = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // cbox_adjective
             // 
             this.cbox_adjective.AutoSize = true;
-            this.cbox_adjective.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbox_adjective.Location = new System.Drawing.Point(168, 168);
+            this.cbox_adjective.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_adjective.Location = new System.Drawing.Point(119, 278);
             this.cbox_adjective.Margin = new System.Windows.Forms.Padding(1);
             this.cbox_adjective.Name = "cbox_adjective";
-            this.cbox_adjective.Size = new System.Drawing.Size(84, 21);
+            this.cbox_adjective.Size = new System.Drawing.Size(83, 20);
             this.cbox_adjective.TabIndex = 0;
             this.cbox_adjective.Text = "Adjective";
             this.cbox_adjective.UseVisualStyleBackColor = true;
@@ -60,11 +65,11 @@
             // cbox_animal
             // 
             this.cbox_animal.AutoSize = true;
-            this.cbox_animal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbox_animal.Location = new System.Drawing.Point(168, 88);
+            this.cbox_animal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_animal.Location = new System.Drawing.Point(119, 198);
             this.cbox_animal.Margin = new System.Windows.Forms.Padding(1);
             this.cbox_animal.Name = "cbox_animal";
-            this.cbox_animal.Size = new System.Drawing.Size(69, 21);
+            this.cbox_animal.Size = new System.Drawing.Size(68, 20);
             this.cbox_animal.TabIndex = 1;
             this.cbox_animal.Text = "Animal";
             this.cbox_animal.UseVisualStyleBackColor = true;
@@ -72,11 +77,11 @@
             // cbox_verb
             // 
             this.cbox_verb.AutoSize = true;
-            this.cbox_verb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbox_verb.Location = new System.Drawing.Point(168, 116);
+            this.cbox_verb.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_verb.Location = new System.Drawing.Point(119, 226);
             this.cbox_verb.Margin = new System.Windows.Forms.Padding(1);
             this.cbox_verb.Name = "cbox_verb";
-            this.cbox_verb.Size = new System.Drawing.Size(57, 21);
+            this.cbox_verb.Size = new System.Drawing.Size(56, 20);
             this.cbox_verb.TabIndex = 2;
             this.cbox_verb.Text = "Verb";
             this.cbox_verb.UseVisualStyleBackColor = true;
@@ -84,39 +89,39 @@
             // cbox_noun
             // 
             this.cbox_noun.AutoSize = true;
-            this.cbox_noun.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbox_noun.Location = new System.Drawing.Point(168, 143);
+            this.cbox_noun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_noun.Location = new System.Drawing.Point(119, 253);
             this.cbox_noun.Margin = new System.Windows.Forms.Padding(1);
             this.cbox_noun.Name = "cbox_noun";
-            this.cbox_noun.Size = new System.Drawing.Size(61, 21);
+            this.cbox_noun.Size = new System.Drawing.Size(59, 20);
             this.cbox_noun.TabIndex = 3;
             this.cbox_noun.Text = "Noun";
             this.cbox_noun.UseVisualStyleBackColor = true;
             // 
             // tbox_username
             // 
-            this.tbox_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbox_username.Location = new System.Drawing.Point(168, 240);
+            this.tbox_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbox_username.Location = new System.Drawing.Point(194, 437);
             this.tbox_username.Margin = new System.Windows.Forms.Padding(1);
             this.tbox_username.Name = "tbox_username";
-            this.tbox_username.Size = new System.Drawing.Size(162, 23);
+            this.tbox_username.Size = new System.Drawing.Size(173, 22);
             this.tbox_username.TabIndex = 4;
             this.tbox_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // tbox_password
             // 
-            this.tbox_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbox_password.Location = new System.Drawing.Point(168, 278);
+            this.tbox_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbox_password.Location = new System.Drawing.Point(194, 475);
             this.tbox_password.Margin = new System.Windows.Forms.Padding(1);
             this.tbox_password.Name = "tbox_password";
-            this.tbox_password.Size = new System.Drawing.Size(162, 23);
+            this.tbox_password.Size = new System.Drawing.Size(173, 22);
             this.tbox_password.TabIndex = 5;
             this.tbox_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btn_generate
             // 
-            this.btn_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_generate.Location = new System.Drawing.Point(168, 329);
+            this.btn_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_generate.Location = new System.Drawing.Point(217, 557);
             this.btn_generate.Margin = new System.Windows.Forms.Padding(1);
             this.btn_generate.Name = "btn_generate";
             this.btn_generate.Size = new System.Drawing.Size(95, 28);
@@ -126,86 +131,141 @@
             // 
             // btn_copy_username
             // 
-            this.btn_copy_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_copy_username.Location = new System.Drawing.Point(346, 236);
+            this.btn_copy_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_copy_username.Location = new System.Drawing.Point(383, 433);
             this.btn_copy_username.Margin = new System.Windows.Forms.Padding(1);
             this.btn_copy_username.Name = "btn_copy_username";
-            this.btn_copy_username.Size = new System.Drawing.Size(42, 22);
+            this.btn_copy_username.Size = new System.Drawing.Size(65, 27);
             this.btn_copy_username.TabIndex = 7;
             this.btn_copy_username.Text = "Copy";
             this.btn_copy_username.UseVisualStyleBackColor = true;
             // 
             // btn_copy_password
             // 
-            this.btn_copy_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_copy_password.Location = new System.Drawing.Point(346, 275);
+            this.btn_copy_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_copy_password.Location = new System.Drawing.Point(383, 473);
             this.btn_copy_password.Margin = new System.Windows.Forms.Padding(1);
             this.btn_copy_password.Name = "btn_copy_password";
-            this.btn_copy_password.Size = new System.Drawing.Size(42, 22);
+            this.btn_copy_password.Size = new System.Drawing.Size(65, 27);
             this.btn_copy_password.TabIndex = 8;
             this.btn_copy_password.Text = "Copy";
             this.btn_copy_password.UseVisualStyleBackColor = true;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(168, 207);
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(194, 370);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(1);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(159, 23);
+            this.numericUpDown1.Size = new System.Drawing.Size(170, 22);
             this.numericUpDown1.TabIndex = 9;
             this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lbl_username
             // 
             this.lbl_username.AutoSize = true;
-            this.lbl_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_username.Location = new System.Drawing.Point(81, 243);
+            this.lbl_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_username.Location = new System.Drawing.Point(110, 440);
             this.lbl_username.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lbl_username.Name = "lbl_username";
-            this.lbl_username.Size = new System.Drawing.Size(77, 17);
+            this.lbl_username.Size = new System.Drawing.Size(74, 16);
             this.lbl_username.TabIndex = 10;
             this.lbl_username.Text = "Username:";
             // 
             // lbl_password
             // 
             this.lbl_password.AutoSize = true;
-            this.lbl_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_password.Location = new System.Drawing.Point(81, 278);
+            this.lbl_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.Location = new System.Drawing.Point(110, 475);
             this.lbl_password.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(73, 17);
+            this.lbl_password.Size = new System.Drawing.Size(71, 16);
             this.lbl_password.TabIndex = 11;
             this.lbl_password.Text = "Password:";
             // 
             // lbl_length_of_password
             // 
             this.lbl_length_of_password.AutoSize = true;
-            this.lbl_length_of_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_length_of_password.Location = new System.Drawing.Point(22, 206);
+            this.lbl_length_of_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_length_of_password.Location = new System.Drawing.Point(59, 369);
             this.lbl_length_of_password.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lbl_length_of_password.Name = "lbl_length_of_password";
-            this.lbl_length_of_password.Size = new System.Drawing.Size(136, 17);
+            this.lbl_length_of_password.Size = new System.Drawing.Size(127, 16);
             this.lbl_length_of_password.TabIndex = 12;
             this.lbl_length_of_password.Text = "Length of password:";
             // 
-            // lbl_preference
+            // lbl_username_preference
             // 
-            this.lbl_preference.AutoSize = true;
-            this.lbl_preference.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_preference.Location = new System.Drawing.Point(29, 88);
-            this.lbl_preference.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.lbl_preference.Name = "lbl_preference";
-            this.lbl_preference.Size = new System.Drawing.Size(129, 17);
-            this.lbl_preference.TabIndex = 13;
-            this.lbl_preference.Text = "Names preference:";
+            this.lbl_username_preference.AutoSize = true;
+            this.lbl_username_preference.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_username_preference.Location = new System.Drawing.Point(102, 166);
+            this.lbl_username_preference.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl_username_preference.Name = "lbl_username_preference";
+            this.lbl_username_preference.Size = new System.Drawing.Size(142, 16);
+            this.lbl_username_preference.TabIndex = 13;
+            this.lbl_username_preference.Text = "Username preference:";
+            // 
+            // lbl_password_preference
+            // 
+            this.lbl_password_preference.AutoSize = true;
+            this.lbl_password_preference.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password_preference.Location = new System.Drawing.Point(329, 166);
+            this.lbl_password_preference.Name = "lbl_password_preference";
+            this.lbl_password_preference.Size = new System.Drawing.Size(139, 16);
+            this.lbl_password_preference.TabIndex = 14;
+            this.lbl_password_preference.Text = "Password preference:";
+            // 
+            // cbox_uppercase
+            // 
+            this.cbox_uppercase.AutoSize = true;
+            this.cbox_uppercase.Location = new System.Drawing.Point(347, 200);
+            this.cbox_uppercase.Name = "cbox_uppercase";
+            this.cbox_uppercase.Size = new System.Drawing.Size(104, 17);
+            this.cbox_uppercase.TabIndex = 15;
+            this.cbox_uppercase.Text = "Uppercase (A-Z)";
+            this.cbox_uppercase.UseVisualStyleBackColor = true;
+            // 
+            // cbox_lowercase
+            // 
+            this.cbox_lowercase.AutoSize = true;
+            this.cbox_lowercase.Location = new System.Drawing.Point(347, 228);
+            this.cbox_lowercase.Name = "cbox_lowercase";
+            this.cbox_lowercase.Size = new System.Drawing.Size(101, 17);
+            this.cbox_lowercase.TabIndex = 16;
+            this.cbox_lowercase.Text = "Lowercase (a-z)";
+            this.cbox_lowercase.UseVisualStyleBackColor = true;
+            // 
+            // cbox_number
+            // 
+            this.cbox_number.AutoSize = true;
+            this.cbox_number.Location = new System.Drawing.Point(347, 255);
+            this.cbox_number.Name = "cbox_number";
+            this.cbox_number.Size = new System.Drawing.Size(87, 17);
+            this.cbox_number.TabIndex = 17;
+            this.cbox_number.Text = "Number (0-9)";
+            this.cbox_number.UseVisualStyleBackColor = true;
+            // 
+            // cbox_special
+            // 
+            this.cbox_special.AutoSize = true;
+            this.cbox_special.Location = new System.Drawing.Point(347, 280);
+            this.cbox_special.Name = "cbox_special";
+            this.cbox_special.Size = new System.Drawing.Size(121, 17);
+            this.cbox_special.TabIndex = 18;
+            this.cbox_special.Text = "Special (!@#$%^&*-.)";
+            this.cbox_special.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 391);
-            this.Controls.Add(this.lbl_preference);
+            this.ClientSize = new System.Drawing.Size(545, 635);
+            this.Controls.Add(this.cbox_special);
+            this.Controls.Add(this.cbox_number);
+            this.Controls.Add(this.cbox_lowercase);
+            this.Controls.Add(this.cbox_uppercase);
+            this.Controls.Add(this.lbl_password_preference);
+            this.Controls.Add(this.lbl_username_preference);
             this.Controls.Add(this.lbl_length_of_password);
             this.Controls.Add(this.lbl_password);
             this.Controls.Add(this.lbl_username);
@@ -243,7 +303,12 @@
         private System.Windows.Forms.Label lbl_username;
         private System.Windows.Forms.Label lbl_password;
         private System.Windows.Forms.Label lbl_length_of_password;
-        private System.Windows.Forms.Label lbl_preference;
+        private System.Windows.Forms.Label lbl_username_preference;
+        private System.Windows.Forms.Label lbl_password_preference;
+        private System.Windows.Forms.CheckBox cbox_uppercase;
+        private System.Windows.Forms.CheckBox cbox_lowercase;
+        private System.Windows.Forms.CheckBox cbox_number;
+        private System.Windows.Forms.CheckBox cbox_special;
     }
 }
 
